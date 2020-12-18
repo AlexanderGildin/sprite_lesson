ebec03115b76d537cbca7dcb0eca658f52c89ea5
Алим <amulla2004@gmail.com>
2020-12-18 15:50:49 +0500

    something

---
+++ main.py
@@ -0,0 +1,14 @@
+from pygame import *
+import pygame
+import os
+
+WIDTH = 500
+HEIGHT = 500
+SCREEN = pygame.display.set_mode(HEIGHT)
+PATH = os.path.dirname(__file__)
+class Arrow:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pygame.init()
